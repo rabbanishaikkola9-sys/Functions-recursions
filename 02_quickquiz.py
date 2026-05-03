cc6bea33b75caff13d42a4dcb68a1c6ca61c932c
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-05-03 09:09:23 +0530

    added

---
+++ 02_quickquiz.py
@@ -0,0 +1,4 @@
+name=input("Enter the user name :")
+def greet():  # userdefined function
+    print(f"Good afternoon {name}")
+greet()
---
+++ 03_function_with_args.py
@@ -0,0 +1,4 @@
+# name=input("Enter the user name :")
+def greet(name):  # userdefined function with arguments
+    print(f"Good afternoon {name}")
+greet("Rabbani")
