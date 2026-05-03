00e8039f9d219f3e4d122cd225b6be88c7b94e04
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-05-03 09:11:01 +0530

    Update 03_function_with_args.py

--- 03_function_with_args.py
+++ 03_function_with_args.py
@@ -1,4 +1,7 @@
 # name=input("Enter the user name :")
-def greet(name):  # userdefined function with arguments
+def greet(name,ending):  # userdefined function with arguments
     print(f"Good afternoon {name}")
-greet("Rabbani")
+    print(ending)
+greet("Rabbani","Thank you")
+greet("Shreya","Thanks!!")
+greet("Naveen","Thank you")
